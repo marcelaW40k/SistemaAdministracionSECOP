fa08ee7318123d86b454cd83c62e2bb075284aab
marcelaW40k <ardilamarcela42@gmail.com>
2026-08-25 01:44:00 -0500

    modificando CorsConfingSource

--- backend/src/main/java/com/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm ->
                         sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
