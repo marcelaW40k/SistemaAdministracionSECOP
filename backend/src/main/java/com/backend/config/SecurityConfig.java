0d69ed2430595fc685998d7e196e7d7ca922f54e
marcelaW40k <ardilamarcela42@gmail.com>
2026-08-24 23:45:25 -0500

    Modificando el security

--- backend/src/main/java/com/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/config/SecurityConfig.java
@@ -73,11 +73,11 @@ public class SecurityConfig {
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
-
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
