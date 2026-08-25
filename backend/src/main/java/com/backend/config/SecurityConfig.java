0d5dd535865b48cc27d5c4675764e3a41048fa9a
marcelaW40k <ardilamarcela42@gmail.com>
2026-08-25 00:30:39 -0500

    origin de CORS sin path

--- backend/src/main/java/com/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://determined-connection-production-5df4.up.railway.app/login","http://localhost:5173", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://determined-connection-production-5df4.up.railway.app/","http://localhost:5173", "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
